dc44bc5a3eaaf8653206fa3ad6bfba2e952a4726
AnanasHerz <ilja.speicher@gmail.com>
2023-07-31 23:22:28 +0200

    README and comment's added

---
+++ README.md
@@ -0,0 +1,12 @@
+# Visit at
+
+[https://ananasherz.github.io/freevbucks/](https://ananasherz.github.io/freevbucks/)
+
+# Usage
+
+The usage of this website is just to redirect to my most recent long-form YouTube video
+
+# BTS / How it's working
+
+The website is fetching the top 10 most recent videos and looks for the first video that doesn't have `#shorts` in its title.
+Finally its redirecting the user to the video in question.
--- main.js
+++ main.js
@@ -1,20 +1,18 @@
 const loadVideo = meta => {
-    fetch('https://api.rss2json.com/v1/api.json?rss_url=https://www.youtube.com/feeds/videos.xml?channel_id=UCEWIYszywEy8TK4xgfrQbPA')
+    fetch('https://api.rss2json.com/v1/api.json?rss_url=https://www.youtube.com/feeds/videos.xml?channel_id=UCEWIYszywEy8TK4xgfrQbPA')  //YT-API
     .then(response => response.json())
     .then(result => {
-        console.log(result)
         let index = 0
-        var link = result.items[index].link
-        var title = result.items[index].title
-        console.log(link)
-        while (title.includes('#shorts')) {
+        var link = result.items[index].link             //Link
+        var title = result.items[index].title           //Title
+        while (title.includes('#shorts')) {             //Checks the title until it doesn't include #shorts
             index++
             link = result.items[index].link
             title = result.items[index].title
-            console.log(link)
         }
+        console.log(link)                               //Final Link
         const meta = document.querySelector("meta")
-        meta.setAttribute('content', `0; url=${link}`)
+        meta.setAttribute('content', `0; url=${link}`)  //Sets the redirect element to the correct link
     })
     .catch(error => console.log('error', error))
 }
